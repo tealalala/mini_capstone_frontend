8eb7a34dea679b5580596839f0e69efa44fddd1f
Tea <tdinhla@protonmail.com>
2018-11-20 18:09:31 -0800

    add bootstrap cdn to public > index.html and delete CSS from app.vue

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,40 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h1>{{ message }}</h1>
+
+    <ul>
+      <li v-for="product in products">
+        <p>id: {{ product.id }}</p>
+        <p>name: {{ product.name }}</p>
+        <p>price: {{ product.price }}</p>
+        <p>description: {{ product.description }}</p>
+        <p>is_discounted: {{ product.is_discounted }}</p>
+      </li>
+    </ul>
+
   </div>
 </template>
 
+<style>
+</style>
+
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+var axios = require("axios");
 
 export default {
-  name: 'home',
-  components: {
-    HelloWorld
-  }
-}
+  data: function() {
+    return {
+      message: "Mini Capstone - Products Index",
+      products: []
+    };
+  },
+  created: function() {
+    axios.get('http://localhost:3000/api/products').then(function(response) {
+      console.log(response.data);
+      this.products = response.data;
+    }.bind(this))
+  },
+  methods: {},
+  computed: {}
+};
 </script>
